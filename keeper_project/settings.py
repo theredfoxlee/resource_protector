6ef102b32b64e2d1874a77d6df855d166c34c708
Kamil Janiec <kamil.p.janiec@gmail.com>
2021-06-22 10:41:21 +0200

    add django-cleanup / s3-file deletion-sync

--- keeper_project/settings.py
+++ keeper_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'keeper'
+    'keeper',
+    'django_cleanup.apps.CleanupConfig', # should be the last
 ]
 
 MIDDLEWARE = [
